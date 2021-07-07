87cf821dd41efbf4ec032103835b0dde554b4e03
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-07-07 10:41:51 -0300

    Começando projeto

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
